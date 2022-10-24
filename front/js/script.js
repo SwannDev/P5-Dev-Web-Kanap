82facca09e7b2a2cf171400e40669a77eedbe58d
SwannDev <61378466+SwannDev@users.noreply.github.com>
2022-10-24 14:17:00 +0200

    Get information from API
    - Display images
    - Display information about kanap
    - Link to id page of product is functional

--- front/js/script.js
+++ front/js/script.js
@@ -1,15 +1,37 @@
-const img = document.getElementById('img') 
+// Fonction permetant de retourner tous les produits de l'API
+async function getProducts() { 
+  try {
+      let response = await fetch('http://localhost:3000/api/products')
+      // conversion de la réponse au format json    
+      return await response.json();
+  }
+  catch (e) {
+      console.log("Erreur lors de l'appel du serveur " + e);
+      alert("Erreur lors de l'appel du serveur " );   // Affichage utilisateur
+  } 
+};
 
 
-let items = {
-    productName: 'Kanap 2x2',
-    productDescription: 'Canapé familial',
-    productID: 42,
-    img: 
-  };
+// Fonction permettant d'afficher les produits
+(async function displayProducts() {
+  let products = await getProducts();
   
-img.addEventListener('click', function() {
-   // console.log('yes');
+  //---creation du rendu
+  let render = '';
+  products.forEach(item => {
+      let htmlContentItem = `
+          <a href="./product.html?id=${item._id}">
+              <article>
+                  <img src="${item.imageUrl}" alt="${item.altTxt}">
+                  <h3 class="productName">${item.name}</h3>
+                  <p class="productDescription">${item.description}</p>
+              </article>
+          </a>
+      `;
+      // stocker le rendu en memoire
+      render = render+htmlContentItem;  
+  });
+  // injection du rendu sur la page
+  document.getElementById('items').innerHTML = render;
+})();
 
-    img.classList.add('show')
-    alert('Ca a changé')
